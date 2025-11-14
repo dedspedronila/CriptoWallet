c74a4030be871b699bc279e0fba68abb5d19359c
André Pedronila <andreluiz.pedronila@gmail.com>
2025-11-14 10:00:52 -0300

    Criação de novos arquivos na pasta model

--- CriptoWallet/src/main/java/com/empresa/criptowallet/model/Criptomoeda.java
+++ CriptoWallet/src/main/java/com/empresa/criptowallet/model/Criptomoeda.java
@@ -5,5 +5,47 @@ package com.empresa.criptowallet.model;
 import java.math.BigDecimal; //Usamos para valores que mexem com dinheiro, para evitar erros de ponto flutuante.
 
 public class Criptomoeda {
+    private String simbolo; //A sigla da criptomoeda.
+    private String nome; //Nome da sigla da criptomoeda.
+    private BigDecimal cotacaoBRL; //a cotacao atual em BRL
+    private BigDecimal cotacaoUSD; //a cotacao atual em USD
+    
+    public Criptomoeda(String simbolo, String nome){
+        this.simbolo = simbolo;
+        this.nome = nome;
+        this.cotacaoBRL = cotacaoBRL;
+        this.cotacaoUSD = cotacaoUSD;
+    }
 
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getCotacaoBRL() {
+        return cotacaoBRL;
+    }
+
+    public void setCotacaoBRL(BigDecimal cotacaoBRL) {
+        this.cotacaoBRL = cotacaoBRL;
+    }
+
+    public BigDecimal getCotacaoUSD() {
+        return cotacaoUSD;
+    }
+
+    public void setCotacaoUSD(BigDecimal cotacaoUSD) {
+        this.cotacaoUSD = cotacaoUSD;
+    }
 }
---
+++ CriptoWallet/src/main/java/com/empresa/criptowallet/model/Transacao.java
@@ -0,0 +1,11 @@
+package com.empresa.criptowallet.model;
+
+//Andre
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transacao {
+    //Enum é usado é para 
+    public enum TipoTransacao{
+    }
+}
